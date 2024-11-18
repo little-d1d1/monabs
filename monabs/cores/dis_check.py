957b2d3a2d7c2a12c3d4ba4f2e381463a4c396f6
rainoftime <rainoftime@gmail.com>
2024-11-18 18:10:03 +0800

    unary check

--- monabs/cores/dis_check.py
+++ monabs/cores/dis_check.py
@@ -24,7 +24,6 @@ def compact_check_misc(precond: z3.ExprRef, cnt_list: [z3.ExprRef], res_label: L
                 res_label[i] = 0
     elif s_res == z3.sat:
         m = sol.model()
-        # models.append(m)  # counterexample
         for i in range(len(res_label)):
             if res_label[i] == 2 and z3.is_true(m.eval(cnt_list[i], True)):
                 res_label[i] = 1
@@ -44,10 +43,48 @@ def disjunctive_check(precond: z3.ExprRef, cnt_list: List[z3.ExprRef]) -> List[i
     >>> pre = x > 100
     >>> f1 = x > 0; f2 = And(x > y, x < y); f3 = x < 3
     >>> cnts = [f1, f2, f3]
-    >>> assert compact_check(pre, cnts) == [1, 0, 0]
+    >>> assert disjunctive_check(pre, cnts) == [1, 0, 0]
     """
-    res = []
-    for _ in cnt_list:
-        res.append(2)
+    res = [2] * len(cnt_list)  # 0 means unsat, 1 means at, 2 means "unknown"
     compact_check_misc(precond, cnt_list, res)
+    return res
+
+
+def compact_check_misc_incremental(solver: z3.Solver, precond: z3.ExprRef, cnt_list: List[z3.ExprRef], res_label: List[int]):
+    f = z3.BoolVal(False)
+    for i, label in enumerate(res_label):
+        if label == 2:
+            f = z3.Or(f, cnt_list[i])
+
+    if z3.is_false(f):
+        return
+
+    solver.push()
+    solver.add(f)
+    s_res = solver.check()
+
+    if s_res == z3.unsat:
+        for i in range(len(res_label)):
+            if res_label[i] == 2:
+                res_label[i] = 0
+    elif s_res == z3.sat:
+        m = solver.model()
+        for i in range(len(res_label)):
+            if res_label[i] == 2 and z3.is_true(m.eval(cnt_list[i], model_completion=True)):
+                res_label[i] = 1
+
+    solver.pop()
+    compact_check_misc_incremental(solver, precond, cnt_list, res_label)
+
+
+def disjunctive_check_incremental(precond: z3.ExprRef, cnt_list: List[z3.ExprRef]) -> List[int]:
+    """
+    Given a precondition G and a set of constraints: f1, f2, ..., fn
+    Decide if the following constraints are satisfiable:
+           And(G, f1), And(G, f2), ..., And(G, fn)
+    """
+    res = [2] * len(cnt_list)
+    solver = z3.Solver()
+    solver.add(precond)
+    compact_check_misc_incremental(solver, precond, cnt_list, res)
     return res
